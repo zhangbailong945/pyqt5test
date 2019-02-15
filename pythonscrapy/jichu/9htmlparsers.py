4d28d8282efdf6372fa0ef4c5f9a462a13a146a7
zhangbailong945 <1207549344@qq.com>
2019-02-15 23:33:34 +0800

    zh update

--- pythonscrapy/jichu/9htmlparsers.py
+++ pythonscrapy/jichu/9htmlparsers.py
@@ -5,30 +5,46 @@ from urllib.request import Request,urlopen
 
 class MyHTMLParser(HTMLParser):
 
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.readingtitle=0
+        self.title=''
+        self.htmls=[]
+
     def handle_starttag(self,tag,attrs):
-        print('%s'%tag)
+
+        for attr in attrs:
+            print(attr)
+        if tag=='title':
+            self.readingtitle=1
     
     def handle_endtag(self,tag):
-        print('%s'%tag)
+        if tag=='title':
+            self.readingtitle=0
     
     def handle_startendtag(self,tag,attrs):
-        print('%s'% tag)
+        pass
+        
     
     def handle_data(self,data):
-        print('data:%s'%data)
+        if self.readingtitle:
+            self.title+=data
     
     def handle_comment(self,data):
-        print('comment:%s'%data)
+        pass
     
     def handle_entityref(self,name):
-        print('&#%s'% name)
+        pass
     
     def handle_charref(self,name):
-        print('&#%s'% name)
+        pass
+    
+    def getTitle(self):
+        return self.title
 
 
 if __name__=='__main__':
-    url='http://www.baidu.com'
+    url='https://www.baidu.com'
     headers={
         'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Mobile Safari/537.36',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -38,3 +54,4 @@ if __name__=='__main__':
     parser=MyHTMLParser()
     parser.feed(response.read().decode('utf-8'))
     response.close()
+    print('title:%s'%(parser.getTitle()))
