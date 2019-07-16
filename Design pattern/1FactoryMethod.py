ba911b348904f071fc5b1f210cc4bd80c0287f08
zhangbailong945 <1207549344@qq.com>
2019-07-16 17:49:06 +0800

    am

--- Design pattern/1FactoryMethod.py
+++ Design pattern/1FactoryMethod.py
@@ -9,7 +9,7 @@ class CheryCar:
     def create(self):
         print('生产'+self.car_name)
 
-class GeelyCar():
+class GeelyCar:
     '''
     吉利模具
     '''
@@ -21,20 +21,25 @@ class GeelyCar():
         print('生产'+self.car_name)
 
 
-def Car(classname):
+class CarFactory:
     '''
-    造车 \n
+    造车工厂 \n
     工程模式接口函数 \n
-    @param classname 车模具
+    @param ctype 车模具
     '''
-    product=dict(Chery=CheryCar,Geely=GeelyCar)
-    return product[classname]()
+    @staticmethod
+    def createCar(ctype):
+        cars=dict(Chery=CheryCar,Geely=GeelyCar)
+        try:
+            return cars[ctype]()
+        except KeyError:
+            print('没有找该到[%s]的车模具,不能生产！'% ctype)
 
 
 if __name__ == "__main__":
     #生产奇瑞
-    car1=Car('Chery')
+    car1=CarFactory.createCar('Chery')
     car1.create()
     #生产吉利
-    car2=Car('Geely')
+    car2=CarFactory.createCar('Geely')
     car2.create()
